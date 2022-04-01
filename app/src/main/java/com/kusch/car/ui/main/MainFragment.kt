5f2894452b991b14fefd18474e084b8e0670e39c
kusch ivan <i-kush@mail.ru>
2022-04-01 11:05:10 +0700

    Машина едет к произвольно намеченой точке, через одну другую произвольную точку

--- app/src/main/java/com/kusch/car/ui/main/MainFragment.kt
+++ app/src/main/java/com/kusch/car/ui/main/MainFragment.kt
@@ -1,32 +1,78 @@
 package com.kusch.car.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.kusch.car.R
+import com.kusch.car.databinding.MainFragmentBinding
+
 
 class MainFragment : Fragment() {
-
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
+    private var screenWidth = 0
+    private var screenHeight = 0
+    private var _binding: MainFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        _binding = MainFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+        val trackField = requireActivity().findViewById<FrameLayout>(R.id.track_field)
+        trackField.viewTreeObserver.addOnGlobalLayoutListener { initView(trackField) }
+    }
+
+    private fun initView(trackField: FrameLayout) {
+        screenWidth = trackField.width
+        screenHeight = trackField.height
+        binding.imgCar.setOnClickListener {
+            viewModel.initTrack(Pair(binding.imgCar.x, binding.imgCar.y))
+        }
+        binding.imgCar.x = screenWidth / 10.0F
+        binding.imgCar.y = (screenHeight - 200).toFloat()
+
+        viewModel.setScreenSize(screenWidth, screenHeight)
+        viewModel.setCarSize(binding.imgCar.width)
+        viewModel.ldTargetPosition.observe(viewLifecycleOwner) {
+            binding.imgTarget.x = it.first + binding.imgCar.width / 2
+            binding.imgTarget.y = it.second + binding.imgCar.height / 2
+        }
+        viewModel.ldTrack.observe(viewLifecycleOwner) {
+            moveToPoint(it.angDegree, it.target, it.rotateMove)
+        }
+    }
+
+    private fun moveToPoint(
+        angDegree: Float,
+        target: Pair<Float, Float>,
+        rotateMove: Pair<Float, Float>
+    ) {
+        binding.imgCar.animate().x(binding.imgCar.x + rotateMove.first)
+            .y(binding.imgCar.y + rotateMove.second).rotation(angDegree)
+            .setDuration(1000).withEndAction {
+                binding.imgCar.animate().x(target.first).y(target.second)
+                    .setDuration(2000).withEndAction { viewModel.calcNextTarget() }
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
--- app/src/main/java/com/kusch/car/ui/main/MainViewModel.kt
+++ app/src/main/java/com/kusch/car/ui/main/MainViewModel.kt
@@ -1,7 +1,79 @@
 package com.kusch.car.ui.main
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.random.Random
+
+data class Track(
+    val angDegree: Float,
+    val target: Pair<Float, Float>,
+    val rotateMove: Pair<Float, Float>
+)
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private var screenWidth = 0
+    private var screenHeight = 0
+    var ldTargetPosition = MutableLiveData<Pair<Float, Float>>()
+    private var curPosition = Pair(0F, 0F)
+    var ldTrack = MutableLiveData<Track>()
+    private var pointCount = 1
+    private var carSize = 70
+
+    private var angDegree = 0.0.toFloat()
+
+    private fun moveTarget() = Pair(
+        carSize + Random.nextInt(screenWidth - carSize * 2).toFloat(),
+        carSize + Random.nextInt(screenHeight - carSize * 2).toFloat()
+    )
+
+    fun setCarSize(carSize: Int) {
+        this.carSize = carSize
+    }
+
+    fun setScreenSize(width: Int, height: Int) {
+        screenWidth = width
+        screenHeight = height
+    }
+
+    fun initTrack(curPosition: Pair<Float, Float>) {
+        this.curPosition = curPosition
+        ldTargetPosition.value = moveTarget()
+        pointCount = 1
+        calcTrackParams()
+    }
+
+    fun calcTrackParams() {
+        val target = if (pointCount == 0) ldTargetPosition.value else moveTarget()
+        var curX = curPosition.first
+        var curY = curPosition.second
+        var targetX = target!!.first
+        var targetY = target!!.second
+
+        val deltaX = targetX - curX
+        val deltaY = targetY - curY
+        val hopoten = Math.sqrt(
+            Math.pow(
+                (curY - targetY).toDouble(),
+                2.0
+            ) + Math.pow((targetX - curX).toDouble(), 2.0)
+        )
+
+        angDegree = Math.toDegrees(Math.acos((curY - targetY) / hopoten)).toFloat()
+        if ((deltaX < 0 && deltaY < 0) || (deltaX < 0 && deltaY > 0))
+            angDegree = -angDegree
+
+        ldTrack.value = Track(
+            angDegree,
+            Pair(targetX, targetY),
+            Pair((deltaX / 3.0).toFloat(), (deltaY / 3.0).toFloat())
+        )
+        curPosition = Pair(targetX, targetY)
+    }
+
+    fun calcNextTarget() {
+        if (pointCount > 0) {
+            pointCount--
+            calcTrackParams()
+        }
+    }
 }
